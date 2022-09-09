f13f02933c08665deb91b095327f97c082e69b9e
Kelsey Green <KelseyL15@aol.com>
2022-09-09 14:41:08 -0400

    commit tree d02726dfe1c1f5875acb12c268753fe80fe77ab9

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+ 
+    cats.push("Milo", "Otis", "Garfield");
+  });
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop(-1);
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+}
+function appendCat(name) {
+    const newCats = [...cats];
+    newCats.push(name);           
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [...cats];
+    newCats.unshift(name);
+    return newCats;
+   
+}
+function removeLastCat() {
+    const newCats = [...cats];
+    newCats.splice(2);
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = [...cats];
+    newCats.shift(0);
+    return newCats;
+}
